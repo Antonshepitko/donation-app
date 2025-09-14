15c45503995d4dfcfafe4dea61beb88a44dbab48
Antonshepitko <71402820+Antonshepitko@users.noreply.github.com>
2025-09-14 20:48:26 +0300

    id fix

--- models/donation.go
+++ models/donation.go
@@ -10,3 +10,13 @@ type Donation struct {
 	Message   string    `json:"message" bson:"message"`
 	Timestamp time.Time `json:"timestamp" bson:"timestamp"`
 }
+
+type Donation struct {
+    ID        primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+    Amount    float64            `json:"amount" bson:"amount"`
+    Currency  string             `json:"currency" bson:"currency"`
+    Name      string             `json:"name" bson:"name"`
+    Message   string             `json:"message" bson:"message"`
+    Streamer  string             `json:"streamer" bson:"streamer"`
+    Timestamp time.Time          `json:"timestamp" bson:"timestamp"`
+}
