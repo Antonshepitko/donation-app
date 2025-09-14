7da169b83ea4abb17a85555330f2064eca44dbd3
Antonshepitko <71402820+Antonshepitko@users.noreply.github.com>
2025-09-14 22:31:37 +0300

    typo fix

--- models/donation.go
+++ models/donation.go
@@ -5,16 +5,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 ) 
 
-
-type Donation struct {
-	Streamer  string    `json:"streamer" bson:"streamer"`
-	DonorName string    `json:"donorName" bson:"donorName"`
-	Amount    float64   `json:"amount" bson:"amount"`
-	Currency  string    `json:"currency" bson:"currency"`
-	Message   string    `json:"message" bson:"message"`
-	Timestamp time.Time `json:"timestamp" bson:"timestamp"`
-}
-
 type Donation struct {
     ID        primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
     Amount    float64            `json:"amount" bson:"amount"`
